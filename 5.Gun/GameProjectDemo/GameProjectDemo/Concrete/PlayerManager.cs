654746ad42fbe757f02184bdcef9824aade058a8
mfkahraman <mfurkankahraman@gmail.com>
2022-06-13 19:53:42 +0300

    13 Haziran 2022 - Yazılımcı Geliştrme Kampı 5.Ödev

---
+++ 5.Gun/GameProjectDemo/GameProjectDemo/Concrete/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using GameProjectDemo.Abstract;
+using GameProjectDemo.Concrete;
+using GameProjectDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProjectDemo.Concrete
+{
+    public class PlayerManager : BasePlayerManager
+    {
+        IPlayerCheckService playerCheckManager = new PlayerCheckManager();
+        public override void Register(Player player)
+        {
+            if (playerCheckManager.CheckIfRealPerson(player)==true)
+            {
+               base.Register(player);
+            }
+            else
+            {
+                Console.WriteLine(player.FirstName+ " "+player.LastName+" isimli kullanıcının bilgileri doğrulanamadı.");
+            }
+            
+        }
+    }
+}
